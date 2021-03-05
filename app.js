7df4ba0e499c099794056a6c7bb96c0a21d8a67e
dejotb <dejotb@gmail.comclear>
2021-03-05 23:50:33 +0100

    added burger

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-var menu = document.querySelector(".nav__list");
-var ham = document.querySelector(".hamburger");
-// var xIcon = document.querySelector(".xIcon");
+const menu = document.querySelector(".nav__list");
+const ham = document.querySelector(".hamburger");
+
 
 
 ham.addEventListener("click", toggleMenu);
@@ -10,17 +10,17 @@ ham.addEventListener("click", toggleMenu);
 function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
       menu.classList.remove("showMenu");
-    //   xIcon.style.display = "none";
     } else {
       menu.classList.add("showMenu");
     }
   }
 
 
-var menuLinks = document.querySelectorAll(".nav__item");
+const menuLinks = document.querySelectorAll(".nav__item");
 
 menuLinks.forEach(
     function(nav__link) {
         nav__link.addEventListener("click", toggleMenu);
     }
   )
+
