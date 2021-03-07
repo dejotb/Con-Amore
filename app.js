cffc3a506510ddd6e8cd207d3c7e485962abb776
dejotb <dejotb@gmail.comclear>
2021-03-07 20:20:42 +0100

    major update

--- app.js
+++ app.js
@@ -2,14 +2,37 @@
 const menu = document.querySelector(".nav__list");
 const ham = document.querySelector(".hamburger");
 
+const header = document.querySelector(".header");
+const main = document.querySelector(".main");
+const mediaQuery = window.matchMedia('(max-width: 600px)')
+
+
+
+// TOGGLE HAMBURGER MENU AND BLUR
+
+ham.addEventListener("click", toggleHam);
+
+function toggleHam() {
+  if (header.classList.contains("blur") && main.classList.contains("blur")) {
+    header.classList.remove("blur");
+    main.classList.remove("blur");
+
+  } else {
+
+    header.classList.add("blur");
+    main.classList.add("blur");
+
+  }
+}
 
 
 ham.addEventListener("click", toggleMenu);
 
-
 function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
       menu.classList.remove("showMenu");
+      header.classList.remove("blur");
+      main.classList.remove("blur");
     } else {
       menu.classList.add("showMenu");
     }
@@ -23,4 +46,8 @@ menuLinks.forEach(
         nav__link.addEventListener("click", toggleMenu);
     }
   )
+
+
+
+
 
