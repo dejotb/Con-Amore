305aca01f29430bb7305530a96f461425380a20c
dejotb <dejotb@gmail.comclear>
2021-05-17 10:32:29 +0200

    JS toggleHam modified

--- app.js
+++ app.js
@@ -2,51 +2,43 @@
 const menu = document.querySelector(".nav__list");
 const ham = document.querySelector(".hamburger");
 const header = document.querySelector(".header-blur");
-const main = document.querySelector(".main-blur");
-
+const main = document.querySelector(".main");
+const menuLinks = document.querySelectorAll(".nav__item");
 // TOGGLE HAMBURGER MENU AND BLUR
 
-ham.addEventListener("click", toggleHam);
-
 function toggleHam() {
-  if (header.classList.contains("blur") && main.classList.contains("blur")) {
-    header.classList.remove("blur");
-    main.classList.remove("blur");
-    ham.classList.remove("iconX");
+  const blurHeader = header.classList.contains("blur");
+  const blurMain = main.classList.contains("blur");
+  const isOpened = ham.getAttribute( 'aria-expanded' ) === 'true';
+  const menuOpen = menu.classList.contains("showMenu")
 
+  console.log(blurHeader, blurMain);
+  if ( !blurHeader && !blurMain) {
+    header.classList.toggle("blur");
+    main.classList.toggle("blur");
+    ham.classList.toggle("iconX");
+    ham.classList.toggle( 'btn-menu_open', !isOpened );
+    ham.setAttribute( 'aria-expanded', String( !isOpened ) );
+  }
+
+  if (menuOpen) {
+    menu.classList.remove("showMenu");
+    header.classList.toggle("blur");
+    main.classList.toggle("blur");
+    ham.classList.toggle("iconX");
   } else {
-
-    header.classList.add("blur");
-    main.classList.add("blur");
-    ham.classList.add("iconX");
-
+    menu.classList.add("showMenu");
   }
 }
 
-ham.addEventListener("click", toggleMenu);
 
-function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-      menu.classList.remove("showMenu");
-      header.classList.remove("blur");
-      main.classList.remove("blur");
-      ham.classList.remove("iconX");
+ham.addEventListener("click", toggleHam);
 
-    } else {
-      menu.classList.add("showMenu");
-    }
-  }
-
-  ham.addEventListener( 'click', function() {
-    const isOpened = ham.getAttribute( 'aria-expanded' ) === 'true';
-    ham.classList.toggle( 'btn-menu_open', !isOpened );
-    ham.setAttribute( 'aria-expanded', String( !isOpened ) );
-  } );
-
-const menuLinks = document.querySelectorAll(".nav__item");
 
 menuLinks.forEach(
-    function(nav__link) {
-        nav__link.addEventListener("click", toggleMenu);
-    }
-  )
+  function(nav__link) {
+      nav__link.addEventListener("click", toggleHam);
+  }
+)
+
+
