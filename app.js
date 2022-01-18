6ec00f29c36ef3d6082008b32b55536439f32dfa
dejotb <dejotb@gmail.com>
2022-01-18 20:06:12 +0100

    cookies popup styling

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 "use strict"
 
-const cookieStorage = {
-  getItem: (key) => {
-    const cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((acc, [key, value]) => ({...acc, [key.trim()]: value }), {});
-    return cookies[key];
-  },
-  setItem: (key, value) => {
-    document.cookie = `${key}=${value}`
+// Allowing cookies storage
+
+const handleCookies = function () {
+  const cookieStorage = {
+    getItem: (key) => {
+      const cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((acc, [key, value]) => ({...acc, [key.trim()]: value }), {});
+      return cookies[key];
+    },
+    setItem: (key, value) => {
+      document.cookie = `${key}=${value}`
+    }
+
+
   }
 
+  const storageType = cookieStorage;
+  const consentPropertyName = 'jdc_consent';
 
+  const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+
+  const saveToSotrage = () => storageType.setItem(consentPropertyName, true);
+
+  window.addEventListener('load', function() {
+
+    const consentPopup = document.querySelector('.consent__popup');
+    const acceptBtn = document.querySelector('.accept');
+
+   const acceptFn = () => {
+     saveToSotrage(storageType);
+     consentPopup.classList.add('hidden');
+   };
+
+   acceptBtn.addEventListener('click', acceptFn);
+
+    if (shouldShowPopup(storageType)) {
+      setTimeout(() => {
+
+        consentPopup.classList.remove('hidden');
+      }, 2000)
+    }
+  })
 }
 
-const storageType = cookieStorage;
-const consentPropertyName = 'jdc_consent';
+handleCookies()
 
-const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
-
-const saveToSotrage = () => storageType.setItem(consentPropertyName, true);
-
-window.addEventListener('load', function() {
-  if (shouldShowPopup()) {
-    const consent = confirm('Agree to the terms and conditions of the site?');
-    if (consent) {
-      saveToSotrage();
-    }
-  }
-})
 
 // gsap animations
 
@@ -51,14 +69,18 @@ const animationElements = function () {
     );
 };
 
+animationElements();
+
+
+// Handling navigation
+
+const handleNav = function() {
 
 const menu = document.querySelector(".nav__list");
 const ham = document.querySelector(".hamburger");
 const header = document.querySelector(".header-blur");
 const main = document.querySelector(".main-blur");
 const menuLinks = document.querySelectorAll(".nav__item");
-
-
 const container = document.querySelector('.container')
 const nav = document.querySelector('.nav')
 const logo = document.querySelector('.nav__logo')
@@ -74,8 +96,12 @@ function toggleHam() {
 
   if ( !blurHeader && !blurMain) {
 
-    header.classList.toggle("blur");
-    main.classList.toggle("blur");
+    setTimeout(() => {
+      header.classList.toggle("blur");
+      main.classList.toggle("blur");
+
+    }, 500);
+
     ham.classList.toggle("iconX");
     ham.classList.toggle( 'btn-menu_open', !isOpened );
     ham.setAttribute( 'aria-expanded', String( !isOpened ) );
@@ -138,11 +164,8 @@ const ob = new IntersectionObserver(obCalltoAction, {target: container, threshol
 ob.observe(container.firstElementChild);
 
 
-// run all functions
-
-const startFunctions = function() {
-  animationElements();
   ham.addEventListener("click", toggleHam);
+
   menuLinks.forEach(
     function(nav__link) {
         nav__link.addEventListener("click", toggleMenu);
@@ -151,4 +174,4 @@ const startFunctions = function() {
 }
 
 
-startFunctions()
+handleNav()
